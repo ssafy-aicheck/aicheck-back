ea04a5307cd2b8fd1e109ae57dfa297823bbcb96
BrokenFinger98 <dbtjsdn980818@gmail.com>
2025-03-26 18:03:07 +0900

    dosc: token.yml 수정

--- back-end/business/src/main/java/com/aicheck/business/domain/account/infrastructure/client/BankAccountClient.java
+++
@@ -1,9 +0,0 @@
-package com.aicheck.business.domain.account.infrastructure.client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "bank")
-public interface BankAccountClient {
-
-
-}
