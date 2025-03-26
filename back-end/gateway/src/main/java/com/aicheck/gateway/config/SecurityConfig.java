45084484669c8b75f11ef2da881bfa0db847e4ae
BrokenFinger98 <dbtjsdn980818@gmail.com>
2025-03-26 20:35:42 +0900

    feat: gateway cors 설정

--- back-end/gateway/src/main/java/com/aicheck/gateway/config/SecurityConfig.java
+++ back-end/gateway/src/main/java/com/aicheck/gateway/config/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.aicheck.gateway.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,5 +26,20 @@ public class SecurityConfig {
 			.csrf(csrf -> csrf.disable())
 			.authorizeExchange(exchange -> exchange.anyExchange().permitAll());
 		return http.build();
+	}
+
+	@Bean
+	public CorsWebFilter corsWebFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOriginPatterns(List.of("*"));
+		config.setAllowedHeaders(List.of("*"));
+		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		config.setExposedHeaders(List.of("Authorization", "X-Chunsun-Authorization", "X-User-ID"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+
+		return new CorsWebFilter(source);
 	}
 }
