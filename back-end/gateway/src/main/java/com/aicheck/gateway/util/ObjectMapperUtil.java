4c2e4e7684e603c0f755de69de5d42ef26818fe6
BrokenFinger98 <dbtjsdn980818@gmail.com>
2025-03-27 15:45:59 +0900

    feat: gateway security exceptionHandler objectMapper json 변환 문제 해결

--- back-end/gateway/src/main/java/com/aicheck/gateway/util/ObjectMapperUtil.java
+++ back-end/gateway/src/main/java/com/aicheck/gateway/util/ObjectMapperUtil.java
@@ -2,12 +2,16 @@ package com.aicheck.gateway.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class ObjectMapperUtil {
-	private static final ObjectMapper objectMapper = new ObjectMapper();
+	private static final ObjectMapper objectMapper = new ObjectMapper()
+		.registerModule(new JavaTimeModule())
+		.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
 	public String toJson(Object object) {
 		try {
