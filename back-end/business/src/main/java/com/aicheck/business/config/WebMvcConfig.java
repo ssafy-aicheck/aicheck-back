15f19eb97062156d0f4c742ac2e8f079f213dbd0
이승우 <swoolee97@ajou.ac.kr>
2025-03-28 01:00:49 +0900

    feat: gateway에서 헤더에 담은 유저 정보 추출하는 Argument resolver 구현

---
+++ back-end/business/src/main/java/com/aicheck/business/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.aicheck.business.config;
+
+import com.aicheck.business.global.auth.resolver.CurrentMemberIdArgumentResolver;
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new CurrentMemberIdArgumentResolver());
+    }
+}
+
