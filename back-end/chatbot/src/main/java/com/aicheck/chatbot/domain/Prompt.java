aa14ad70cbabc5d72bffa968d8aaef4816e55635
BrokenFinger98 <dbtjsdn980818@gmail.com>
2025-04-02 11:28:20 +0900

    feat: chatbot domain 설정

--- back-end/chatbot/src/main/java/com/aicheck/chatbot/domain/Prompt.java
+++ back-end/chatbot/src/main/java/com/aicheck/chatbot/domain/Prompt.java
@@ -12,6 +12,7 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -31,9 +32,26 @@ public class Prompt extends BaseEntity {
 	@Column(name = "birth", nullable = false)
 	private Date birth;
 
+	@Column(name = "category_difficulty", nullable = false, columnDefinition = "TEXT")
+	private String categoryDifficulty;
+
 	@Column(name = "content", nullable = false)
 	private String content;
 
 	@Enumerated(EnumType.STRING)
 	private Gender gender;
+
+	@Builder
+	private Prompt(Long childId, Long managerId, Date birth, String categoryDifficulty, String content, Gender gender) {
+		this.childId = childId;
+		this.managerId = managerId;
+		this.birth = birth;
+		this.categoryDifficulty = categoryDifficulty;
+		this.content = content;
+		this.gender = gender;
+	}
+
+	public void updateCategoryDifficulty(String categoryDifficulty) {
+		this.categoryDifficulty = categoryDifficulty;
+	}
 }
--- back-end/db/mysql/chatbot.sql
+++ back-end/db/mysql/chatbot.sql
@@ -2,13 +2,13 @@ USE chatbot;
 
 CREATE TABLE `prompts`
 (
-    `child_id`    INT UNSIGNED NOT NULL PRIMARY KEY,
-    `manager_id`  INT UNSIGNED NOT NULL,
-    `birth`       DATE     NOT NULL,
-    `category`    JSON     NOT NULL,
-    `content`     TEXT     NOT NULL,
-    `gender`      ENUM('MALE', 'FEMALE') NOT NULL,
-    `created_at`  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `modified_at` DATETIME NULL DEFAULT NULL,
-    `deleted_at`  DATETIME NULL DEFAULT NULL
+    `child_id`            INT UNSIGNED NOT NULL PRIMARY KEY,
+    `manager_id`          INT UNSIGNED NOT NULL,
+    `birth`               DATE     NOT NULL,
+    `category_difficulty` TEXT     NOT NULL,
+    `content`             TEXT     NOT NULL,
+    `gender`              ENUM('MALE', 'FEMALE') NOT NULL,
+    `created_at`          DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `modified_at`         DATETIME NULL DEFAULT NULL,
+    `deleted_at`          DATETIME NULL DEFAULT NULL
 );
