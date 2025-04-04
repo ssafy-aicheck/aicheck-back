cbd1b7d3c4190ba2d531d8041cb4ec10321ca817
이승우 <swoolee97@ajou.ac.kr>
2025-04-04 14:58:20 +0900

    docs: DDL sql 수정

--- back-end/db/mysql/batch.sql
+++ back-end/db/mysql/batch.sql
@@ -4,13 +4,14 @@ CREATE TABLE `schedule`
 (
     `id`                INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `member_id`         INT UNSIGNED NOT NULL,
-    `schedule_id`       INT UNSIGNED NOT NULL,
+    `child_id`          INT UNSIGNED NOT NULL,
     `parent_account_no` VARCHAR(20) NOT NULL,
     `child_account_no`  VARCHAR(20) NOT NULL,
+    `report_id`         VARCHAR(255) NOT NULL,
     `amount`            MEDIUMINT UNSIGNED NOT NULL,
     `start_date`        DATE        NOT NULL,
     `interval`          ENUM('MONTHLY', 'WEEKLY', 'BIWEEKLY') NOT NULL,
     `created_at`        DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `modified_at`       DATETIME NULL DEFAULT NULL,
-    `deleted_at`        DATETIME NULL DEFAULT NULL
+    `deleted_at`        DATETIME NULL DEFAULT NULL,
 );
