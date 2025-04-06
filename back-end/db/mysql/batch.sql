e7475f13d57f01ba63a4ac710027cd09ea0b7d2e
이승우 <swoolee97@ajou.ac.kr>
2025-04-06 19:14:25 +0900

    fix: batch.sql에서 reportId 삭제

--- back-end/db/mysql/batch.sql
+++ back-end/db/mysql/batch.sql
@@ -7,7 +7,6 @@ CREATE TABLE `schedule`
     `child_id`          INT UNSIGNED NOT NULL,
     `parent_account_no` VARCHAR(20) NOT NULL,
     `child_account_no`  VARCHAR(20) NOT NULL,
-    `report_id`         VARCHAR(255) NOT NULL,
     `amount`            MEDIUMINT UNSIGNED NOT NULL,
     `start_date`        DATE        NOT NULL,
     `interval`          ENUM('MONTHLY', 'WEEKLY', 'BIWEEKLY') NOT NULL,
