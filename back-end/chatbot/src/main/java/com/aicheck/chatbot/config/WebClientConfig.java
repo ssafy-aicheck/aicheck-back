1f628410bd93eae388f0b0dc4405e92acb6ddd4b
BrokenFinger98 <dbtjsdn980818@gmail.com>
2025-04-08 02:24:00 +0900

    docs: fastApi BaseURL 설정 정보 파일로 변경

--- back-end/chatbot/src/main/java/com/aicheck/chatbot/config/WebClientConfig.java
+++ back-end/chatbot/src/main/java/com/aicheck/chatbot/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.aicheck.chatbot.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -12,10 +13,13 @@ import lombok.extern.slf4j.Slf4j;
 @Configuration
 public class WebClientConfig {
 
+	@Value("${fast-api.base-url}")
+	private String fastApiBaseUrl;
+
 	@Bean(name = "fastApiWebClient")
 	public WebClient fastApiWebClient() {
 		return WebClient.builder()
-			.baseUrl("https://7c54-211-192-210-7.ngrok-free.app")
+			.baseUrl(fastApiBaseUrl)
 			.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
 			.build();
 	}
