b3704fd5f1c2b8ea6a1b5a138906778740da380b
이승우 <swoolee97@ajou.ac.kr>
2025-04-08 20:53:00 +0900

    feat: 용돈 요청 수락시 송금 실행하는 기능

--- back-end/business/src/main/java/com/aicheck/business/domain/transfer/presentation/dto/TransferRequest.java
+++ back-end/business/src/main/java/com/aicheck/business/domain/transfer/presentation/dto/TransferRequest.java
@@ -1,8 +1,10 @@
 package com.aicheck.business.domain.transfer.presentation.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class TransferRequest {
     private String receiverAccountNo;
     private Long amount;
